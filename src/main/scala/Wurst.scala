4dadb576b937be075ff9ae5d07a7927cc98f9a5d
Thomas Kreyling <Thomas.Kreyling@googlemail.com>
2015-11-07 16:11:01 +0100

    how to flatten nested Option[List[T]]

--- src/main/scala/Wurst.scala
+++ src/main/scala/Wurst.scala
@@ -1,3 +1,13 @@
 class Wurst {
-  def sayWurst = "Wurst!"
+  def sayWurst = {
+
+    val optOfList = {
+      Some(List(1, 2, 3))
+    };
+
+    val flatten  = optOfList.getOrElse(List())
+
+    println(flatten)
+    "Wurst!"
+  }
 }
