3340c33e3170e06f741cc3bb4fdaf6af7b6d1d01
Thomas Kreyling <Thomas.Kreyling@googlemail.com>
2015-11-07 16:41:38 +0100

    added Factory for KnowledgeItem

--- src/main/scala/Exp1.scala
+++ src/main/scala/Exp1.scala
@@ -6,14 +6,20 @@ import org.apache.spark.rdd.RDD
 object Exp1 {
   case class Person(id : Int, firstname : String, lastname : String, knowledgeItems : List[KnowledgeItem])
 
-  object PersonFactory {
-    def fromCSV(line : String) = {
-      val cells = line.split(",")
-      Person(cells(0) toInt, cells(1), cells(2), List())
-    }
+  trait DTOFactory[T] {
+    protected def construct(line: Array[String]) : T
+    def fromCSV(line : String) = construct(line.split(","))
+  }
+
+  object PersonFactory extends DTOFactory[Person] {
+    def construct(line : Array[String]) = Person(line(0) toInt, line(1), line(2), List())
   }
 
   case class KnowledgeItem(personId : Int, name : String, level : String)
+
+  object KnowledgeItemFactory extends DTOFactory[KnowledgeItem] {
+    def construct(line : Array[String]) = KnowledgeItem(line(0) toInt, line(1), line(2))
+  }
 
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("Simple Application").setMaster("local");
@@ -22,7 +28,7 @@ object Exp1 {
     val people = sc.textFile("src/main/resources/persons.csv").map(PersonFactory.fromCSV).map(p => (p.id, p));
     people.collect.foreach(println)
 
-    val knowledgeItems = sc.textFile("src/main/resources/knowledge.csv").map(s => s.split(",")).map(s => KnowledgeItem(s(0) toInt, s(1), s(2))).map(k => (k.personId, k));
+    val knowledgeItems = sc.textFile("src/main/resources/knowledge.csv").map(KnowledgeItemFactory.fromCSV).map(k => (k.personId, k));
     knowledgeItems.collect.foreach(println)
 
     val peopleWithKnowledge = leftOuterJoinList[Int,Person,KnowledgeItem](people, knowledgeItems, (p,k) => p.copy(knowledgeItems = k))
