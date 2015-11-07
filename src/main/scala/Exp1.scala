2eff8f5c5b468d40a287cb678aa1c731f6719a13
Thomas Kreyling <Thomas.Kreyling@googlemail.com>
2015-11-07 16:30:21 +0100

    * introduced KnowledgeItem case class * introduced PersonFactory.fromCSV

--- src/main/scala/Exp1.scala
+++ src/main/scala/Exp1.scala
@@ -4,19 +4,28 @@ import org.apache.spark.SparkConf
 import org.apache.spark.rdd.RDD
 
 object Exp1 {
-  case class Person(id : Int, firstname : String, lastname : String, knowledge : List[String])
+  case class Person(id : Int, firstname : String, lastname : String, knowledgeItems : List[KnowledgeItem])
+
+  object PersonFactory {
+    def fromCSV(line : String) = {
+      val cells = line.split(",")
+      Person(cells(0) toInt, cells(1), cells(2), List())
+    }
+  }
+
+  case class KnowledgeItem(personId : Int, name : String, level : String)
 
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("Simple Application").setMaster("local");
     val sc = new SparkContext(conf);
 
-    val people = sc.textFile("src/main/resources/persons.csv").map(s => s.split(",")).map(s => Person(s(0) toInt, s(1), s(2), List())).map(p => (p.id, p));
+    val people = sc.textFile("src/main/resources/persons.csv").map(PersonFactory.fromCSV).map(p => (p.id, p));
     people.collect.foreach(println)
 
-    val knowledge = sc.textFile("src/main/resources/knowledge.csv").map(s => s.split(",")).map(s => (s(0) toInt, s(1)));
-    knowledge.collect.foreach(println)
+    val knowledgeItems = sc.textFile("src/main/resources/knowledge.csv").map(s => s.split(",")).map(s => KnowledgeItem(s(0) toInt, s(1), s(2))).map(k => (k.personId, k));
+    knowledgeItems.collect.foreach(println)
 
-    val peopleWithKnowledge = leftOuterJoinList[Int,Person,String](people, knowledge, (p,k) => p.copy(knowledge = k))
+    val peopleWithKnowledge = leftOuterJoinList[Int,Person,KnowledgeItem](people, knowledgeItems, (p,k) => p.copy(knowledgeItems = k))
     println(peopleWithKnowledge.toDebugString)
 
     peopleWithKnowledge.collect.foreach(println);
