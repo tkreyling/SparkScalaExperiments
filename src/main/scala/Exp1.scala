952ef9afe64676a1d87488c9f6a0a44eda43ba2b
Thomas Kreyling <Thomas.Kreyling@googlemail.com>
2015-11-12 00:16:02 +0100

    added Type Parameter for enrichment of data type

--- src/main/scala/Exp1.scala
+++ src/main/scala/Exp1.scala
@@ -5,15 +5,15 @@ import scala.reflect.ClassTag
 import scala.Ordering
 
 object Exp1 {
-  case class Person(id : Int, firstname : String, lastname : String, knowledgeItems : List[KnowledgeItem])
+  case class Person[KI<:Option[List[KnowledgeItem]]](id : Int, firstname : String, lastname : String, knowledgeItems : KI)
 
   trait DTOFactory[T] {
     protected def construct(line: Array[String]) : T
     def fromCSV(line : String) = construct(line.split(","))
   }
 
-  object PersonFactory extends DTOFactory[Person] {
-    def construct(line : Array[String]) = Person(line(0) toInt, line(1), line(2), List())
+  object PersonFactory extends DTOFactory[Person[None.type]] {
+    def construct(line : Array[String]) = Person(line(0) toInt, line(1), line(2), None)
   }
 
   case class KnowledgeItem(personId : Int, name : String, level : String)
@@ -32,15 +32,15 @@ object Exp1 {
     val knowledgeItems = sc.textFile("src/main/resources/knowledge.csv").map(KnowledgeItemFactory.fromCSV).map(k => (k.personId, k));
     knowledgeItems.collect.foreach(println)
 
-    val peopleWithKnowledge = leftOuterJoinList[Int,Person,KnowledgeItem](people, knowledgeItems, (p,k) => p.copy(knowledgeItems = k))
+    val peopleWithKnowledge = leftOuterJoinList[Int,Person[None.type],Person[Some[List[KnowledgeItem]]],KnowledgeItem](people, knowledgeItems, (p,k) => p.copy(knowledgeItems = Some(k)))
     println(peopleWithKnowledge.toDebugString)
 
     peopleWithKnowledge.collect.foreach(println);
   }
 
-  def leftOuterJoinList [K:ClassTag,V1:ClassTag,V2:ClassTag]
-  (leftItems: RDD[(K, V1)], rightItems: RDD[(K, V2)], copyConstructor: (V1, List[V2]) => V1)
-  : RDD[(K, V1)] = {
+  def leftOuterJoinList [K:ClassTag,V1:ClassTag,V1X:ClassTag,V2:ClassTag]
+  (leftItems: RDD[(K, V1)], rightItems: RDD[(K, V2)], copyConstructor: (V1, List[V2]) => V1X)
+  : RDD[(K, V1X)] = {
     val groupedRightItems = rightItems.groupByKey.mapValues(_.toList)
 
     leftItems.leftOuterJoin(groupedRightItems)
